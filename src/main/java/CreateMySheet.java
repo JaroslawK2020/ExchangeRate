8c35ba21d779cef226f7e5618b84cd15bf9ff653
JaroslawK2020 <jaroslaw.kornatowski@gmail.com>
2021-02-28 19:43:59 +0100

    Creating cells method update 1.1

--- src/main/java/CreateMySheet.java
+++ src/main/java/CreateMySheet.java
@@ -1,11 +1,9 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class CreateMySheet {
     private CreateExcelFile excelFile = new CreateExcelFile();
@@ -18,7 +16,6 @@ public class CreateMySheet {
 
     public void createHeaders(Map headersName){
         Set<String> headersSet = headersName.keySet();
-//        System.out.println("headers keySet:" + headersSet);
         int rowNumber = 0;
             XSSFRow row = sheet.createRow(rowNumber);
             int cellNumber = 0;
@@ -38,11 +35,15 @@ public class CreateMySheet {
                 headersValues) {
             XSSFCell cell;
             if(value instanceof ArrayList){
+                row.createCell(3).setCellValue(((Map) ((ArrayList<?>) value).get(0)).keySet().toString());
+//                row.createCell(3).setCellValue(((Map) ((ArrayList<?>) value).get(0)).keySet().forEach();//trzeba użyć forEach??
+//                (((Map) ((ArrayList<?>) value).get(0)).keySet()).forEach(System.out::println);
                 for (int i = 0; i < ((ArrayList<?>) value).size(); i++) {
                     row = sheet.createRow(rowNumber+i+1);
 //                    cell = row.createCell(3);
 //                    cell.setCellValue(((ArrayList<?>) value).get(i).toString());
 //                    System.out.println(((Map) ((ArrayList<?>) value).get(i)).entrySet());
+
                     row.createCell(3).setCellValue(((Map) ((ArrayList<?>) value).get(i)).get("no").toString());
                     row.createCell(4).setCellValue(((Map) ((ArrayList<?>) value).get(i)).get("effectiveDate").toString());
                     row.createCell(5).setCellValue(((Map) ((ArrayList<?>) value).get(i)).get("mid").toString());
@@ -54,40 +55,6 @@ public class CreateMySheet {
             }
         }
     }
-
-    //działający kod na tworzenie Cell w 2 linii excela
-    /*public void createCell(Map headersName){
-        Collection headersValues = headersName.values();
-//        System.out.println("headers Value :" + headersValues);
-        int rowNumber = 1;
-        XSSFRow row = sheet.createRow(rowNumber);
-        int cellNumber = 0;
-        for (Object value:
-                headersValues) {
-            XSSFCell cell = row.createCell(cellNumber++);
-            if(cellNumber == 4){//cellNumber 4 to ArrayList które chcę zaimplementować jak mapę
-                break;
-            }
-            cell.setCellValue(value.toString());
-        }
-    }*/
-
-    /*public void createCellsFromArray(ArrayList ratesArray){
-//        System.out.println("ratesArray Class" + ratesArray.getClass());
-//        System.out.println("ratesArray get(0) Class " + ratesArray.get(0).getClass());
-        int rowNum = 1;
-        for (int i = 0; i < ratesArray.size(); i++) {
-            System.out.println("createCellsFromArray i: " + i + " " + ratesArray.get(i).getClass());
-            HashMap hashMap = (HashMap) ratesArray.get(i);
-            for (Object entry:
-                    hashMap.keySet()) {
-                XSSFRow row = sheet.createRow(1);
-                XSSFCell cell = row.createCell(rowNum++);
-                cell.setCellValue(entry.toString());
-            }
-        }
-    }*/
-
 
     public void createMyRowFromArray(Map data){
         Set<String> keyset = data.keySet();
