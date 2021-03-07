e41edfa2dbf2d0c860e87cd4666630f4709d4c92
JaroslawK2020 <jaroslaw.kornatowski@gmail.com>
2021-03-07 19:44:21 +0100

    Backend finally ready 1.0. Scene Builder and JavaFx configured.

---
+++ src/main/java/FrontEnd/window.java
@@ -0,0 +1,26 @@
+package FrontEnd;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class window extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        var javaVersion = SystemInfo.javaVersion();
+        var javafxVersion = SystemInfo.javafxVersion();
+
+        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        var scene = new Scene(new StackPane(label), 640, 480);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
