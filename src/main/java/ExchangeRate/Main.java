8c039369b77507165d686598831376d33ea3f1ed
JaroslawK2020 <jaroslaw.kornatowski@gmail.com>
2021-05-10 11:53:57 +0200

    App finally ready. Executable JAR work.

--- src/main/java/ExchangeRate/Main.java
+++ src/main/java/ExchangeRate/Main.java
@@ -4,10 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
-import java.io.File;
 
 public class Main extends Application {
     @Override
@@ -22,7 +19,7 @@ public class Main extends Application {
 
     /*public static void main(String[] args) {
 
-//        launch(args);
+        launch(args);
     }*/
 
     void caller(String[] args){
